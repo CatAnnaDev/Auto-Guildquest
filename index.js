a15e252d67feff6390f5f69299c26a8ddfc52035
PsykoDev <45910905+PsykoDev@users.noreply.github.com>
2020-04-09 07:41:26 +0200

    rewrite 92.3

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ module.exports = function AutoGuildquest(mod) {
 		clr = 0,
 		entered = false,
 		hold = false,
-		//vquest log
 		daily = 0,
 		weekly = 0,
 		niceName = mod.proxyAuthor !== 'caali' ? '[VG] ' : ''
@@ -27,133 +26,74 @@ module.exports = function AutoGuildquest(mod) {
 			CompleteExtra()
 		}
 	});
-//thx Kygas 
-/*	mod.hook('S_SYSTEM_MESSAGE', 1, (event) => {
-        const msg = mod.parseSystemMessage(event.message);
-        if (msg) {
-              console.log(msg);
-        }
-	}); 
-//GquestLog
-	 mod.hook("S_GUILD_QUEST_LIST", 1, (event) => {
-		if (mod.settings.GQuestLog) {
-			GetQuestsInfo(event["quests"]);
-		}
-	})*/
-//Vquestlog
-	mod.game.on('enter_game', () => {
-		daily = weekly = 0
-	})
 
-	mod.hookOnce('S_AVAILABLE_EVENT_MATCHING_LIST', 1, event => {
-		daily = event.unk4
-		weekly = event.unk6
-	})
-
-//Daily
-	mod.hook('S_LOGIN', 'event', () => {
-		mod.hookOnce('S_SPAWN_ME', 'event', () => {
-			setTimeout(dailycredit,1000+ Math.random()*250);
-		});
-	});
-//Guardian
-	mod.hook('S_FIELD_EVENT_ON_ENTER', 'raw', () => {  
-		entered = true;
-		//return false;
-	});
-//Guardian
-	mod.hook('C_RETURN_TO_LOBBY', 'raw', () => {  
-		entered = false;
-	});
-//Vandguard	
+//Hook
+	mod.game.on('enter_game', () => {daily = weekly = 0})
+	mod.hookOnce('S_AVAILABLE_EVENT_MATCHING_LIST', 1, event => {daily = event.unk4weekly = event.unk6})
+	mod.hook('S_LOGIN', 'event', () => {mod.hookOnce('S_SPAWN_ME', 'event', () => {setTimeout(dailycredit,1000+ Math.random()*250);});});
+	mod.hook('S_FIELD_EVENT_ON_ENTER', 'raw', () => {  entered = true;});
+	mod.hook('C_RETURN_TO_LOBBY', 'raw', () => {  entered = false;});
 	mod.hook('S_COMPLETE_EVENT_MATCHING_QUEST', 1, (event) => {
 		daily++
 		weekly++
 		if (mod.settings.Vanguard) {
 			myQuestId = event.id
 			if (!hold) setTimeout(completeQuest,1000+ Math.random()*250);
-			//return false;
 		}
 	});
-//Guardian
-	mod.hook('S_FIELD_EVENT_PROGRESS_INFO', 1, () => {
-		if (mod.settings.Guardian) setTimeout(completeGuardian, 2000+ Math.random()*250);
-	});
-//Gquest
+	mod.hook('S_FIELD_EVENT_PROGRESS_INFO', 1, () => {if (mod.settings.Guardian) setTimeout(completeGuardian, 2000+ Math.random()*250);});
 	mod.hook('S_UPDATE_GUILD_QUEST_STATUS', 1, (event) => {
 		if (mod.settings.GQuest) {
 			if (event.targets[0].completed == event.targets[0].total) {
 				setTimeout(()=>{
-				mod.send('C_REQUEST_FINISH_GUILD_QUEST', 1, {
-					quest: event.quest
-				})
+				mod.send('C_REQUEST_FINISH_GUILD_QUEST', 1, {quest: event.quest})
 				}, 2000 + Math.random()*1000)
-				
 				setTimeout(() => {
-					mod.send('C_REQUEST_START_GUILD_QUEST', 1, {
-						questId: event.quest
-					})
+				mod.send('C_REQUEST_START_GUILD_QUEST', 1, {questId: event.quest})
 				}, 4000 + Math.random()*1000)
 			}
-			//return false;
 		}
 	})
-//Guardian
 	mod.hook('S_FIELD_POINT_INFO', 2, (event) => {       
-		if(entered && event.cleared != clr && event.cleared - 1 > event.claimed)
-		{
+		if(entered && event.cleared != clr && event.cleared - 1 > event.claimed){
 			mod.toClient('S_CHAT', 3, {
 			channel: 21,
 			gm: 1,
 			name: 'Guardian Mission',
 			message: String(event.cleared + " / 40")
-			});
+			});}clr = event.cleared;});
+/*	 mod.hook("S_GUILD_QUEST_LIST", 1, (event) => {
+		if (mod.settings.GQuestLog) {
+			GetQuestsInfo(event["quests"]);
 		}
-		clr = event.cleared;
-	});
-//Vanguard
+	})*/
+//Function
 	function completeQuest() {
-		mod.send('C_COMPLETE_DAILY_EVENT', 1, {
-			id: myQuestId
-		})	
-		setTimeout(() => {
-			mod.send('C_COMPLETE_EXTRA_EVENT', 1, {
-				type: 0
-			})
+		mod.send('C_COMPLETE_DAILY_EVENT', 1, {id: myQuestId})	
+		setTimeout(() => {mod.send('C_COMPLETE_EXTRA_EVENT', 1, {type: 0})
 		}, 500+ Math.random()*250)
-		setTimeout(() => {
-			mod.send('C_COMPLETE_EXTRA_EVENT', 1, {
-				type: 1
-			})
+		setTimeout(() => {mod.send('C_COMPLETE_EXTRA_EVENT', 1, {type: 1})
 		}, 1000+ Math.random()*250)
 		myQuestId = 0
 		if(mod.settings.VLog) report() 
 		
 	}; 
-
 	function report() {
 		if(daily < 16) mod.command.message(niceName + 'Daily Vanguard Requests completed: ' + daily)
 		else mod.command.message(niceName + 'You have completed all 16 Vanguard Requests today.')
 	}
-
-//Guardian
 	function completeGuardian() {
-		mod.send('C_REQUEST_FIELD_POINT_REWARD', 1, {
-		})
+		mod.send('C_REQUEST_FIELD_POINT_REWARD', 1, {})
 		setTimeout(() => {
-			mod.send('C_REQUEST_ONGOING_FIELD_EVENT_LIST', 1, {
-			})
-		}, 2000+ Math.random()*500)
+		mod.send('C_REQUEST_ONGOING_FIELD_EVENT_LIST', 1, {})
+	}, 2000+ Math.random()*500)
 };
-//Daily
 	function dailycredit() {
 		if (mod.settings.Daily) {
 			let _ = mod.trySend('C_REQUEST_RECV_DAILY_TOKEN', 1, {});
 			 !_ ? mod.log('Unmapped protocol packet \<C_REQUEST_RECV_DAILY_TOKEN\>.') : null;
 		  }
 	};
-
-//GquestLog
 /*	function GetQuestsInfo(questEvent) {
 		for (let questIndex in questEvent) {
 			if ([1, 2].includes(questEvent[questIndex]["status"])) {
@@ -162,12 +102,7 @@ module.exports = function AutoGuildquest(mod) {
 				let qStatus = `${questEvent[questIndex]["status"] == 1 ? "[ACTIVE]".clr("f1ef48") : "[COMPLETE]".clr("3fce29")}`;
 				let qTime = new Date(1000 * questEvent[questIndex]["timeRemaining"]).toISOString().substr(11, 8);
 				mod.command.message(`${qStatus} ${Quests[qName].clr("0cccd6")} ${qSize.clr("0c95d4")} Time left: ${qTime.clr("db3dce")}`)
-		} else {
-			continue
-		}
-	}
-} 
-//GquestLog
+		} else {continue}}} 
 	function GetQuestSize(size) {
 		if (size == 0) {
 			return "(Small)"
@@ -177,10 +112,7 @@ module.exports = function AutoGuildquest(mod) {
 			return "(Large)"
 		}
 	}*/
-
-//Msg
 	function sendMessage(msg) { mod.command.message(msg) }
-//Ui
 	let ui = null;
 	if (global.TeraProxy.GUIMode) {
 		ui = new SettingsUI(mod, require('./settings_structure'), mod.settings, { alwaysOnTop: true, width: 550, height: 232 });
